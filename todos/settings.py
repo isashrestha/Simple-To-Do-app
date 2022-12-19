e9ff2abd259a11d96023a810e1433576c7f2113d
unknown <np01cp4s210038@islingtoncollege.edu.np>
2022-12-19 17:21:30 +0545

    Added sqlite

--- todos/settings.py
+++ todos/settings.py
@@ -76,15 +76,22 @@ WSGI_APPLICATION = "todos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME':'project_todo',
+#         'HOST': 'mysql',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'PORT':3306
+        
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'project_todo',
-        'HOST': 'mysql',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'PORT':3306
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
